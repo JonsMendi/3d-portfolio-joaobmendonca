bc8c57b7d6df2f1c87ffda9c3852b8bd5c48ccc5
João Borges Mendonça <jborgesmendonca@gmail.com>
2023-03-22 13:45:15 +0100

    main: Fixed

--- pages/components/Present.tsx
+++ pages/components/Present.tsx
@@ -6,10 +6,10 @@ const Present = () => {
     <section className={`relative w-full h-screen mx-auto`}>
       <div className={`absolute inset-0 max-w-7xl mx-auto ${styles.paddingX} flex flex-col justify-center z-20`}>
         <div>
-          <h1 className={`${styles.heroHeadText} text-white title-name`}>
+          <h1 className="font-black lg:text-[80px] sm:text-[60px] xs:text-[50px] text-[40px] lg:leading-[98px] mt-2 text-white title-name">
           João<span className='text-[#93896D] title-name'>Borges</span>Mendonça
           </h1>
-          <p className={`${styles.heroSubText} mt-2 text-white-100`}>
+          <p className="text-[#dfd9ff] font-medium lg:text-[30px] sm:text-[26px] xs:text-[20px] text-[16px] lg:leading-[40px] mt-2">
             I build web applications, user interfaces and 3D visuals
           </p>
         </div>
