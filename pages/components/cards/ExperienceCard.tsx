83eb3b913819068ac2708f3d97e6868afeeaec28
João Borges Mendonça <jborgesmendonca@gmail.com>
2023-03-27 12:28:23 +0200

    main: Fixed

--- pages/components/cards/ExperienceCard.tsx
+++ pages/components/cards/ExperienceCard.tsx
@@ -23,13 +23,13 @@ const ExperienceCard: React.FC<Props> = ({ experience }) => {
         background: "#202328",
         color: "#FFFFFF",
       }}
-      date={experience.date}
-      iconStyle={{ background: experience.icon_background }}
+      date={experience?.date}
+      iconStyle={{ background: experience?.icon_background }}
       icon={
         <div className="flex justify-center items-center w-full h-full">
           <Image
-            src={experience.icon}
-            alt={experience.company_name}
+            src={experience?.icon}
+            alt={experience?.company_name}
             className="w-[70%] h-[70%] object-contain"
             width={100}
             height={100}
@@ -40,22 +40,22 @@ const ExperienceCard: React.FC<Props> = ({ experience }) => {
     >
       <div>
         <h3 className="text-[#9f9882] text-[24px] font-bold uppercase">
-          {experience.title}
+          {experience?.title}
         </h3>
         <p
           className="text-secondary text-[16px] font-semibold"
           style={{ margin: 0 }}
         >
-          {experience.company_name}
+          {experience?.company_name}
         </p>
       </div>
       <p className="text-secondary text-[24px] font-bold">
-        {experience.description}
+        {experience?.description}
       </p>
 
       <p className="text-white text-[14px] pl-1 tracking-wider">Tasks:</p>
       <ul className="mt-5 list-disc ml-5 space-y-2">
-        {experience.tasks.map((task, index) => (
+        {experience?.tasks.map((task, index) => (
           <li
             key={`experience-point-${index}`}
             className="text-white text-[14px] pl-1 tracking-wider"
