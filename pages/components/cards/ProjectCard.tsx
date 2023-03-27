66fd1e5679d98a1b708de3709a8b98d7e9f58c9f
João Borges Mendonça <jborgesmendonca@gmail.com>
2023-03-27 12:33:33 +0200

    main: Fixed

--- pages/components/cards/ProjectCard.tsx
+++ pages/components/cards/ProjectCard.tsx
@@ -1,15 +1,13 @@
 import React from "react";
 import { motion as m } from "framer-motion";
-
-
-import { fadeInVariant, titleVariant, containerVariants } from "../../../utilities/motion-framer";
 import Image from "next/image";
+import { fadeInVariant, containerVariants } from "../../../utilities/motion-framer";
 
 interface ProjectCardProps {
   index: number;
   name: string;
   description: string;
-  tags: [];
+  tags: { name: string }[];
   image: string;
   source_code_link: string;
 }
@@ -22,7 +20,6 @@ const ProjectCard = ({
   image,
   source_code_link,
 }: ProjectCardProps) => {
-
   return (
     <m.div variants={fadeInVariant("up", "spring", index * 0.3, 0.75)}>
       <m.div
@@ -31,36 +28,29 @@ const ProjectCard = ({
         onClick={() => window.open(source_code_link, "_blank")}
         className="cursor-pointer "
       >
-        <div
-        className='bg-primary p-5 sm:w-[360px] w-full'
-      >
-        <div className='relative w-full h-[230px]'
-              >
-          <Image
-            src={image}
-            alt='project_image'
-            className='w-full h-full object-cover rounded-2xl'
-          />
-        </div>
+        <div className="bg-primary p-5 sm:w-[360px] w-full">
+          <div className="relative w-full h-[230px]">
+            <Image
+              src={image}
+              alt="project_image"
+              className="w-full h-full object-cover rounded-2xl"
+            />
+          </div>
 
-        <div className='mt-5'>
-          <h3 className='text-tertiary font-bold text-[24px]'>{name}</h3>
-          <p className='mt-2  text-secondary text-[14px]'>{description}</p>
-        </div>
+          <div className="mt-5">
+            <h3 className="text-tertiary font-bold text-[24px]">{name}</h3>
+            <p className="mt-2 text-secondary text-[14px]">{description}</p>
+          </div>
 
-        <div className='mt-4 flex flex-wrap gap-2'>
-          {tags.map((tag: any) => (
-            <p
-              key={`${name}-${tag.name}`}
-              className="text-[14px]" 
-            >
-              #{tag.name}
-            </p>
-          ))}
+          <div className="mt-4 flex flex-wrap gap-2">
+            {tags.map((tag) => (
+              <p key={`${name}-${tag.name}`} className="text-[14px]">
+                #{tag.name}
+              </p>
+            ))}
+          </div>
         </div>
-      </div>
       </m.div>
-      
     </m.div>
   );
 };
